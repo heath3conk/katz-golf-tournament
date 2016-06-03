7c7c73c8e5218bc3f1e942ec3b9486e29aafa8d7
Heather Conklin <heath3conk@gmail.com>
2016-06-03 17:22:17 -0500

    Progress on sponsorship signup form

--- app/models/sponsorship.rb
+++ app/models/sponsorship.rb
@@ -1,7 +1,18 @@
 class Sponsorship < ActiveRecord::Base
 
-  # validates :sponsorship_type, presence: true
-
   belongs_to :signup
+
+  def sponsor_total
+    sponsorship_total = 0
+    if self.tee_box == 0
+      tee_box_total = 0 
+    elsif self.tee_box == 1
+      tee_box_total = 250
+    elsif self.tee_box > 1
+      tee_box_total = (self.tee_box - 1) * 150 + 250
+    end
+    sponsorship_total = (self.buffet * 1200) + (self.beverage_cart * 1000) + (self.at_the_turn * 500) + (self.closest_to_pin * 250) + (self.longest_drive * 250) + tee_box_total
+    sponsorship_total
+  end
 
 end

f91e5ab96b1eaa35200f7b32d09545882a1d96c2
Heather Conklin <heath3conk@gmail.com>
2016-06-13 19:09:44 -0500

    Restrict routes for signups

--- app/controllers/diners_controller.rb
+++ app/controllers/diners_controller.rb
@@ -1,9 +1,22 @@
 class DinersController < ApplicationController
 
+  before_action :authenticate!, only: [:index]
+
   def new
     # start a new signup record
     @signup = Signup.new
     @diners = @signup.diners
+  end
+
+  def index 
+    signups = Signup.all 
+    @diners_signups = []
+    signups.each do |signup|
+      if signup.diners.count > 0
+        @diners_signups << signup
+      end
+    end
+    @diners_signups
   end
 
 end
--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -3,31 +3,24 @@ class SessionController < ApplicationController
   def create
     @user = User.find_by(email: params[:session][:email])
     if @user && @user.authenticate(params[:session][:password])
-      p "user okay"
       login(@user)
-      redirect_to signups_path(@user)
+      redirect_to signups_path
     else
-      p "problem user"
       redirect_to new_session_path(@user), flash: { error: "Invalid credentials, please try again." }
     end
   end
 
   def new
     @user = User.new
-    # redirect_to new_session_path(@user)
-  end
-
-  def destroy
-    logout
-    redirect_to root_url
-  end
-
-  def login(user)
-    session[:user_id] = user.id
   end
 
   def logout
     session.delete(:user_id)
+    redirect_to pages_path
+  end
+
+  def login(user)
+    session[:user_id] = user.id
   end
 
 end
